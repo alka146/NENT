f99e3d9d2ec2728e83c058f69d0ae9ef489ca2d1
alka146 <42872953+alka146@users.noreply.github.com>
2020-01-29 16:06:42 +0100

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -16,6 +16,16 @@ class Input:
         raw_input ()
         return env
     
+    def ReadEnv(self, env):
+            if env.lower() not in ('development', 'experimental', 'production'):
+                print("Not an appropriate choice. Valid choices are Development, Experimental or Production")
+            else:
+                print("You have entered")
+                print env
+                print("Press Enter to go ahead.\n")
+                raw_input ()
+        return env
+    
     def ReadCIDR(self):
         while True:
             cidr = raw_input("Please enter the CIDR you want to have for the VPC:\n")
@@ -29,3 +39,12 @@ class Input:
         raw_input ()
         return cidr
         
+    def ReadCIDR(self, cidr):
+            if re.match("^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$", cidr) is None:
+                print("Not an appropriate entry. Please enter a valid CIDR")
+            else:
+                print("You have entered")
+                print cidr
+                print("Press Enter to go ahead.\n")
+                raw_input ()
+                return cidr
