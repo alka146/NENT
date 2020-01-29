6d60a69e20a3fdaba58d0fa4b38863987bac1762
alka146 <42872953+alka146@users.noreply.github.com>
2020-01-29 16:15:51 +0100

    Update test_userInput.py

--- testing/test_userInput.py
+++ testing/test_userInput.py
@@ -1,1 +1,19 @@
+import unittest
+from user_input import Input
 
+class TestUserInput(unittest.Testcase):
+  
+  def test_ReadEnv1(self):
+    env = Input.ReadEnv(abc)
+    self.assertEqual(env, INVALID)
+  
+  def test_ReadEnv2(self):
+    env = Input.ReadEnv(Development)
+    self.assertEqual(env, Development)
+  
+  def test_ReadEnv3(self):
+    env = Input.ReadEnv(prod)
+    self.assertEqual(env, INVALID)
+
+if __name__ == '__main__':
+  unittest.main()
