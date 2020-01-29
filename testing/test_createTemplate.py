9260fd92f3e360262de1c69c28b978d43539658c
alka146 <42872953+alka146@users.noreply.github.com>
2020-01-29 16:19:56 +0100

    Update test_createTemplate.py

--- testing/test_createTemplate.py
+++ testing/test_createTemplate.py
@@ -1,1 +1,11 @@
+import unittest
+from create_template import Create
 
+class TestCreateTemplate(unittest.Testcase):
+  
+  def test_ReadEnv1(self):
+    cfn = Create.CreateTemplate(abc)
+    self.assertEqual(env, INVALID)
+
+if __name__ == '__main__':
+  unittest.main()
